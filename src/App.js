b7ac26b62b4aeacf8f3feab903362da7658f6c7f
mazurenka <an.drej@mail.ru>
2022-03-16 20:01:49 +0100

    websocket hooks chat

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
 
-    let sendMessage = () => {
-        alert('Hey')
+    const [message, setMessage] = useState('')
+    const [users, setUsers] = useState([
+        {id: 1, name: 'Andre', photo: 'https://via.placeholder.com/50', message: 'Yo!'}
+    ])
+
+    let onMessageChange = (e) => {
+        setMessage(e.currentTarget.value)
     }
 
-     return (
+    let sendMessage = () => {
+        alert(message)
+    }
+
+    return (
         <div className="App">
 
             <div className={'chat'}>
                 <div className={'messages'}>
-                    <div className={'message'}>
-                        <img/> <b>Dima</b> <span>Hello guys</span>
-                    </div>
-
-                    <div className={'message'}>
-                        <img/> <b>Andre</b> <span>Hello guys</span>
-                    </div>
-
+                    {users.map(u => <div className={'message'}>
+                        <img src={u.photo}/> <b>{u.name}</b> <span>{u.message}</span>
+                    </div>)}
                 </div>
                 <div className={'footer'}>
-                    <textarea/>
-                    <button onClick={sendMessage}>send</button>
+                    <textarea onChange={onMessageChange}>{message}</textarea>
+                    <button onClick={sendMessage}>Send</button>
                 </div>
             </div>
 
