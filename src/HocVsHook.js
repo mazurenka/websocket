469ff22c0cc89e13539b0ec19a1ba8f164ccb5d7
mazurenka <an.drej@mail.ru>
2022-03-21 13:54:21 +0100

    hoc vs hook

--- src/HocVsHook.js
+++ src/HocVsHook.js
@@ -6,13 +6,41 @@ export function HocVsHook() {
         <div className="App">
             <div className={'chat'}>
                 <Select values={['Minsk', "Moscow", 'Kiev']}/>
+                <Textarea limit={10} value={'Yo'} placeholder={'Comment'}/>
             </div>
         </div>
     );
 }
 
-export const Textarea = ({limit}) => {
-    return <textarea/>
+export const Textarea = ({limit, value, ...props}) => {
+    let divStyles = {
+        position: 'relative',
+        width: '300px',
+        height: '100px'
+    }
+    let textareaStyles = {
+        position: 'absolute',
+        width: '300px',
+        height: '100px'
+    }
+    let spanStyles = {
+        position: 'absolute',
+        right: '3px',
+        bottom: '3px'
+    }
+    const [currentValue, setValue] = useState(value)
+    const onChange = (e) => {
+        setValue(e.currentTarget.value)
+    }
+
+    return <div style={divStyles}>
+        <textarea maxLength={limit}
+                  onChange={onChange}
+                  value={currentValue}
+                  {...props}
+                  style={textareaStyles}/>
+        <span style={spanStyles}>{limit}</span>
+    </div>
 }
 
 export const Select = ({values}) => {
