edd398ffab30f8c2b349f86da76142b600bf801f
mazurenka <an.drej@mail.ru>
2022-03-22 13:20:29 +0100

    hooks

--- src/App2.js
+++ src/App2.js
@@ -2,22 +2,52 @@ import React, {useState} from "react";
 
 
 export function App2() {
-    const [counter, setCounter]=useState(0)
-
+    const [counter, setCounter] = useState(0)
+    const [year, setYear] = useState(2022)
+    console.log('App render')
     return (
         <div className={'App'}>
-            <Counter counter={counter} />
+            <Counter counter={counter}/>
+            <Buttons onPlusClick={() => {
+                setCounter(counter + 1)
+            }}/>
+            <Footer year={year}/>
         </div>
     )
 }
 
 function Counter(props) {
+    console.log('Counter render')
     return (
-        <div className={"App"}>
+        <div className={'App'}>
             {props.counter}
         </div>
     )
 }
+
+function Buttons(props) {
+    console.log('Buttons rendered')
+    return (
+        <div>
+            <button onClick={props.onPlusClick}>+</button>
+        </div>
+    )
+}
+
+const Footer = React.memo(function Footer(props) {
+    console.log('Footer Render')
+    return (
+        <div>
+            FOOTER {props.year}
+        </div>
+    )
+})
+
+
+
+
+
+
 
 
 
